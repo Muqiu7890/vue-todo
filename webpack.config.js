587115ca32875aab97c675c6f30693c795bc6a9d
Muqiu7890 <1337651840@qq.com>
2019-07-17 17:48:57 +0800

    packaging third party library

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,17 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const WebpackDevServer = require('webpack-dev-server');
+// 将CSS提取为独立的文件的插件，对每个包含css的js文件都会创建一个CSS文件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === "development"
 
 const config = {  //导出一个具有特殊属性的对象
     target: "web",
-    entry: path.join(__dirname,'./src/index.js'),
+    entry: path.join(__dirname, './src/index.js'),
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist')
+        filename: 'bundle.[hash:8].js',
+        path: path.resolve(__dirname, 'dist')
     },
     mode: 'none',
     module: {
@@ -32,7 +34,7 @@ const config = {  //导出一个具有特殊属性的对象
                 use: [
                     {
                         loader: 'url-loader',
-                        options:{
+                        options: {
                             limit: 1024,
                             name: '[name].[ext]'
                         }
@@ -48,13 +50,13 @@ const config = {  //导出一个具有特殊属性的对象
         }),
         new HtmlWebpackPlugin({
             // 指定生成的文件依赖的html文件模板
-            template:__dirname + '/src/index.html',
+            template: __dirname + '/src/index.html',
         }),
         new VueLoaderPlugin(),
     ]
 };
 
-if(isDev) {
+if (isDev) {
     config.module.rules.push({
         test: /\.styl/,
         use: [
@@ -76,7 +78,49 @@ if(isDev) {
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
     )
-
+} else {
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue'] // 打包类库
+    }
+    config.output.filename = "[name].[chunkhash:8].js"
+    config.module.rules.push(
+        {
+            test: /\.styl/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {publicPath: '../'}
+                },
+                'css-loader',
+                'stylus-loader'
+            ]
+        }
+    )
+    config.plugins.push(
+        new MiniCssExtractPlugin({filename: 'style.[contentHash:8].css'})
+    )
+    // 打包类库相关文件
+    config.optimization = {
+        splitChunks: {
+            cacheGroups: { // 这里开始设置缓存的 chunks
+                commons: {
+                    chunks: 'initial',      // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    minSize: 0,             // 最小尺寸，默认0,
+                    minChunks: 2,           // 最小 chunk ，默认1
+                    maxInitialRequests: 5   // 最大初始化请求书，默认1
+                },
+                vendor: {
+                    test: /node_modules/,   // 正则规则验证，如果符合就提取 chunk
+                    chunks: 'initial',      // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    name: 'vendor',         // 要缓存的 分隔出来的 chunk 名称
+                    priority: 10,           // 缓存组优先级
+                    enforce: true
+                }
+            },
+        },
+        runtimeChunk: true
+    }
 }
 
 module.exports = config;
