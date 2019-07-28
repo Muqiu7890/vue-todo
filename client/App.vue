276631e590244f96ae27a40ef5fa7eb52aa63d00
Muqiu7890 <1337651840@qq.com>
2019-07-28 13:27:42 +0800

    change style

--- client/App.vue
+++ client/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <div id="cover"></div>
         <Header/>
-        <transition name="fade">
+        <transition name="fade" mode="out-in">
             <router-view/>
         </transition>
         <Footer/>
